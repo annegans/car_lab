fe01d505ab8211d43051819fd073cbc93149d033
Anne Ganzeboom <anneganzeboom@gmail.com>
2015-04-30 18:05:31 +0100

    solution number 7

--- src/car.js
+++ src/car.js
@@ -10,7 +10,9 @@ function Car(make, model, year, color,state, previousOwners, currentOwner, passe
 
 Car.prototype.sale = function(newOwner){
 this.previousOwners.push(this.currentOwner);
+this.currentOwner = newOwner ;
 };
+
 
 Car.prototype.paint = function(newColor){
 
